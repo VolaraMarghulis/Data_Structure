c4bb9f34171518b56748279043129675344a5b42
Volara <valeriu.juc1@gmail.com>
2025-05-13 15:42:39 +0300

    change binarySearchTree

--- src/binarySearchTree/BinarySearchTree.java
+++ src/binarySearchTree/BinarySearchTree.java
@@ -1,4 +1,56 @@
 package binarySearchTree;
 
 public class BinarySearchTree {
+    public Node root;
+
+
+    class Node {
+        public int value;
+        public Node left; // pointer to left
+        public Node right; // pointer to right
+        //Node constructor
+        Node(int value) {
+            this.value = value;
+        }
+    }
+    // method for insert
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
+            root = newNode;
+            return true;
+        }
+        Node temp = root;
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
+                    temp.left = newNode;
+                    return true;
+                }
+                temp = temp.left;
+            } else {
+                if (temp.right == null) {
+                    temp.right = newNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+    // method to search through three
+    public boolean contains(int value) {
+        if (root == null) return false;
+        Node temp = root;
+        while (temp != null) {
+            if (value < temp.value) {
+                temp = temp.left;
+            } else if (value > temp.value) {
+                temp = temp.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
